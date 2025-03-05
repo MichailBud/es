6b8954960a8140b0d3f1c77c88ca7c582ec8e3a3
Михаил Будылин <bmichailabc@gmail.com>
2025-03-05 18:42:53 +0300

    Лаба 1 (Светодиод моргает!)

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,11 @@
-//==============================================================================
+#include <libopencm3/stm32/rcc.h> // reset and clock control
+#include <libopencm3/stm32/gpio.h>  // general purpose input output (общего назначения)
+
 int main() {
-
+    rcc_periph_clock_enable(RCC_GPIOD);
+    gpio_mode_setup(GPIOD, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE,GPIO15);
     while (true) {
-
+        gpio_toggle(GPIOD, GPIO15); // Переключение сигнала
+        for (volatile uint32_t i=0; i < 2'000'000; ++i);
     }
 }
