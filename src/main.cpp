812292a4b505762d931f4e156de8055f899c5d41
Михаил Будылин <bmichailabc@gmail.com>
2025-04-12 13:50:38 +0300

    Рабочий ШИМ на таймере + управление светодиодом

--- src/main.cpp
+++ src/main.cpp
@@ -14,21 +14,48 @@
 
 // Rcc clock_system;
 
-int main() {
-    rcc_periph_clock_enable(RCC_GPIOD); // Включаем групппу портов ввода вывода D
-    gpio_mode_setup(GPIOD, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE,GPIO15); // Активируем 15 вывод
+int compare = 0;
 
-    rcc_periph_clock_enable(RCC_TIM6); // Активируем 6 таймер
-    timer_set_prescaller(TIM6, 8000-1); // Установка предделителя
-    timer_set_period(TIM6, 1000-1); // Установка предела счёта
-    timer_enable_counter(TIM6); // Активацтя таймера
+void timer_enable(rcc_periph_clken rcc_tim, unsigned int tim, int presc, int period){
+    rcc_periph_clock_enable(rcc_tim); // Активируем тактирование таймера?
+    timer_set_prescaler(tim, presc-1); // Установка предделителя
+    timer_set_period(tim, period-1); // Установка предела счёта
+    timer_enable_counter(tim); // Активация таймера
+}
+
+
+
+int main() {
+
+    timer_enable(RCC_TIM4, TIM4, 80, 1000);   // Активация и настройка 4 таймера
+    timer_enable(RCC_TIM6, TIM6, 16000, 1000);   // Активация и настройка 6 таймера
+
+    rcc_periph_clock_enable(RCC_GPIOD); // Включаем групппу портов ввода вывода D
+    gpio_mode_setup(GPIOD, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO15); // Активируем 15 вывод
+    gpio_set_af(GPIOD, GPIO_AF2, GPIO15);
+
+
 
     timer_enable_irq(TIM6, TIM_DIER_UIE); // Разрешает внутри МК отправлять сигнал
-
     nvic_enable_irq(NVIC_TIM6_DAC_IRQ);
+
+    timer_set_oc_mode(TIM4, TIM_OC4, TIM_OCM_PWM1); // Режим ШИМ
+    timer_set_oc_value(TIM4, TIM_OC4, 100); // Значение таймера при котором сработает флаг, но счетчик не обнулится и будет заполняться дальше
+    timer_enable_oc_output(TIM4, TIM_OC4);
+
+    // timer_enable_counter(TIM4);
+    // timer_enable_counter(TIM6);
+
+    while (1);
 }
 
 void tim6_dac_isr (void){
     timer_clear_flag(TIM6, TIM_SR_UIF);
-    gpio_toggle(GPIOD, GPIO15);
+    if (compare >= 1000) {
+        compare = 0;
+    }
+    else {
+        compare += 200;
+    }
+    timer_set_oc_value(TIM4, TIM_OC4, compare);
 }
