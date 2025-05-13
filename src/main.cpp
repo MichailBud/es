f98c14dada874623e1a29684cc009a071629e3c7
Михаил Будылин <bmichailabc@gmail.com>
2025-05-13 18:22:52 +0300

    Успешно отправляем данные по UART

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,37 @@
 #include <libopencm3/stm32/rcc.h> // reset and clock control
 #include <libopencm3/stm32/gpio.h>  // general purpose input output (общего назначения)
-#include  <libopencm3/stm32/timer.h>
+#include  <libopencm3/stm32/usart.h>
 
 #include  <libopencm3/cm3/nvic.h>
 
 
-void timer_enable(rcc_periph_clken rcc_tim, uint32_t tim, uint32_t presc, uint32_t period){
-    rcc_periph_clock_enable(rcc_tim); // Активируем тактирование таймера?
-    timer_set_prescaler(tim, presc-1); // Установка предделителя
-    timer_set_period(tim, period-1); // Установка предела счёта
-    timer_enable_counter(tim); // Активация таймера
-}
 
 int main() {
 
-    rcc_periph_clock_enable(RCC_GPIOA); // Включаем группу портов ввода вывода D
-    gpio_mode_setup(GPIOA, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO0); // Активируем 0 вывод в режиме альтернативной функции
-    gpio_set_af(GPIOA, GPIO_AF2, GPIO0); // Включаем альтернативную функцию порта (таймер)
+    rcc_periph_clock_enable(RCC_GPIOA); // Включаем группу портов ввода вывода A
+    gpio_mode_setup(GPIOA, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO2 | GPIO3); // Активируем 2 и 3 вывод в режиме альтернативной функции
+    gpio_set_af(GPIOA, GPIO_AF7, GPIO2 | GPIO3); // Включаем альтернативную функцию порта (UART) 2(TX), 3(RX)
+
+    // Настройка UART
+    rcc_periph_clock_enable(RCC_USART2);  // Подаём тактовый сигнал
+
+    usart_set_baudrate(USART2, 115200); // Скорость передачи
+    usart_set_databits(USART2, 8); // Размер посылки
+    usart_set_parity(USART2, USART_PARITY_NONE);  // Проверка на чётность имеется или нет
+    usart_set_stopbits(USART2, USART_STOPBITS_1); // Кол-во стопбитов
+
+    usart_set_mode(USART2, USART_MODE_TX_RX); // Режим приёмопередатчика
+    usart_set_flow_control(USART2, USART_FLOWCONTROL_NONE);
+
+    usart_enable(USART2);
 
 
 
     while (true){
+
+        usart_send_blocking(USART2, 'A');
+        usart_send_blocking(USART2, 10);
+        for (volatile uint32_t i = 0; i < 50000; ++i);
     }
 }
 
